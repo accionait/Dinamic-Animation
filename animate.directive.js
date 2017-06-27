a4841e842c4fed54e4845d3c57e2a2c6b1e0011c
Matias Serrano <matias.serrano@acciona-server.com>
2017-06-27 15:49:08 -0300

    fixes menores

--- animate.directive.js
+++ animate.directive.js
@@ -24,28 +24,34 @@
 
         function link(scope, element, attrs){
             //TODO hide post animacion
+            debugger;
             if(scope.init) {
                 element.addClass('ng-hide');
             }
             scope.$watch('trigger', function(newValue, oldValue){
-                if(newValue.length) {
-                    for (var i = 0; i < newValue.length; i++) {
-                        console.log("newValue " + newValue + "   oldValue " + oldValue);
+                debugger;
+                if(newValue != undefined) {
+                    if (newValue.length) {
+                        for (var i = 0; i < newValue.length; i++) {
+                            if (newValue[i] == true) {
+                                if (scope.time != undefined) {
+                                    if (scope.time.length) {
+                                        if (scope.time[i] != null && scope.time[i] != "") {
+                                            var animation = scope.animation[i];
+                                            var out = scope.out == undefined ? undefined : scope.out[i];
+                                            if (scope.infinite != undefined) {
+                                                if (scope.infinite.length) {
 
-                        if(newValue[i] == true) {
-                            if (scope.time != undefined) {
-                                if (scope.time.length) {
-                                    if (scope.time[i] != null && scope.time[i] != "") {
-                                        var animation = scope.animation[i];
-                                        var out = scope.out == undefined ? undefined : scope.out[i];
-                                        if (scope.infinite != undefined)
-                                        {
-                                            if (scope.infinite.length) {
-
-                                                if (scope.infinite[i]) {
-                                                    setInterval(function () {
-                                                        applyClass(animation, out);
-                                                    }, scope.time[i]);
+                                                    if (scope.infinite[i]) {
+                                                        setInterval(function () {
+                                                            applyClass(animation, out);
+                                                        }, scope.time[i]);
+                                                    }
+                                                    else {
+                                                        setTimeout(function () {
+                                                            applyClass(animation, out);
+                                                        }, scope.time[i]);
+                                                    }
                                                 }
                                                 else {
                                                     setTimeout(function () {
@@ -58,13 +64,11 @@
                                                     applyClass(animation, out);
                                                 }, scope.time[i]);
                                             }
+
                                         }
                                         else {
-                                            setTimeout(function () {
-                                                applyClass(animation, out);
-                                            }, scope.time[i]);
+                                            applyClass(scope.animation[i], scope.out == undefined ? undefined : scope.out[i]);
                                         }
-
                                     }
                                     else {
                                         applyClass(scope.animation[i], scope.out == undefined ? undefined : scope.out[i]);
@@ -74,40 +78,37 @@
                                     applyClass(scope.animation[i], scope.out == undefined ? undefined : scope.out[i]);
                                 }
                             }
-                            else {
-                                applyClass(scope.animation[i], scope.out == undefined ? undefined : scope.out[i]);
-                            }
                         }
                     }
-                }
-                else
-                {
-                    if(newValue == true){
-                        if (scope.time != undefined && scope.time != null && scope.time != "") {
-                            var animation = scope.animation;
-                            var out = scope.out;
-                            if (scope.infinite) {
-                                setInterval(function () {
-                                    applyClass(animation, out);
-                                }, scope.time);
+                    else {
+                        if (newValue == true) {
+                            if (scope.time != undefined && scope.time != null && scope.time != "") {
+                                var animation = scope.animation;
+                                var out = scope.out;
+                                if (scope.infinite) {
+                                    setInterval(function () {
+                                        applyClass(animation, out);
+                                    }, scope.time);
+                                }
+                                else {
+                                    setTimeout(function () {
+                                        applyClass(animation, out);
+                                    }, scope.time);
+                                }
                             }
                             else {
-                                setTimeout(function () {
-                                    applyClass(animation, out);
-                                }, scope.time);
+                                applyClass(scope.animation, out);
                             }
-                        }
-                        else {
-                            applyClass(scope.animation);
                         }
                     }
                 }
             });
 
             function applyClass(animation, out) {
+                debugger;
                 scope.onAnimation = true;
                 var animationAux = animation;
-                var outAux = out;
+                var outAux = out == undefined ? false : out;
                 var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
                 if (outAux == false)
@@ -117,6 +118,7 @@
 
                 if(!element.hasClass('ng-hide')) {
                     element.addClass('animated ' + animationAux).one(animationEnd, function () {
+                        debugger;
                         element.removeClass('animated ' + animationAux);
                         scope.onAnimation = false;
                         scope.$apply();
